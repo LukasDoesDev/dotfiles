c6c49ce49d90adf5ef3f239aa1a4a13f234c10c6
Lukas Does Dev <51633285+LukasDoesDev@users.noreply.github.com>
2021-02-05 23:44:43 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ basedir="$HOME/dev"
 repourl="https://github.com/LukasDoesDev/dotfiles.git"
 
 cd $basedir
-git clone $repourl && cd dotfiles && source bootstrap.sh
+git clone $repourl
+cd dotfiles
+source bootstrap.sh
 
 } # This ensures the entire script is downloaded.
